573c98c2b572b1410d3e869771625847670c1253
Caner Aydın <caner.aydd@gmail.com>
2020-04-29 23:53:18 +0300

    First initialize

--- src/main/java/com/caner/issuemanagement/dto/ProjectDto.java
+++ src/main/java/com/caner/issuemanagement/dto/ProjectDto.java
@@ -1,5 +1,12 @@
 package com.caner.issuemanagement.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProjectDto {
     private Long id;
     private String projectName;
