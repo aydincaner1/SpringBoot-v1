d42e2d7d288f1bbcc633b35662ed2149f4d27c63
Caner Aydın <caner.aydd@gmail.com>
2020-06-02 21:03:25 +0300

    First initialize

--- src/test/java/com/caner/issuemanagement/IssueManagementApplicationTests.java
+++ src/test/java/com/caner/issuemanagement/IssueManagementApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IssueManagementApplicationTests {
+public class IssueManagementApplicationTests {
 
 	@Test
 	public void contextLoads() {
